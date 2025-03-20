472b00fc3034dea3fd40353b73c1cb09e1b79b6a
Najeeb Ibrahim <najeebibraheem1999@hotmail.com>
2025-03-20 15:59:01 +0200

    Update TicketBookingRepository.java

--- src/main/java/com/att/tdp/popcorn_palace/repositories/TicketBookingRepository.java
+++ src/main/java/com/att/tdp/popcorn_palace/repositories/TicketBookingRepository.java
@@ -1,6 +1,6 @@
 package com.att.tdp.popcorn_palace.repositories;
 
-import com.att.tdp.popcorn_palace.domain.TicketBookingEntity;
+import com.att.tdp.popcorn_palace.domain.entities.TicketBookingEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
